e0defd94e4c123bfe2e798ef665553fd2728ab24
lightweigh <827907347@qq.com>
2023-12-24 16:29:11 +0800

    add pca_svm to model.py

--- code/model.py
+++ code/model.py
@@ -1,10 +1,103 @@
 import json
 
+from time import time
+import matplotlib.pyplot as plt
+from scipy.stats import loguniform
 import torch
 import numpy as np
 from torch import nn
 import torch.nn.functional as F
 from torchvision.models.resnet import resnet50
+from sklearn.decomposition import PCA
+from sklearn.metrics import ConfusionMatrixDisplay, classification_report
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+class SVM:
+    def __init__(self, num_classes, h=128, w=128):
+        self.num_classes = num_classes
+        self.class_pos = None
+        self.eigenfaces = None
+        self.pca = None
+        self.clf = None
+        self.h = h
+        self.w = w
+
+    def rgb2gray(self, rgb):
+        r, g, b = rgb[:, 0, :, :], rgb[:, 1, :, :], rgb[:, 2, :, :]
+        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        # print('rgb.shape: ', rgb.shape)
+        # print('gray.shape: ', gray.shape)
+        return gray.reshape(gray.shape[0], -1)
+
+    def fit4clf_cross_validation(self, X, y, dup_times=5):
+        X_dup = []
+        y_dup = []
+        for idx in range(X.shape[0]):
+            for i in range(dup_times):
+                X_dup.append(X[idx])
+                y_dup.append(y[idx])
+        return torch.FloatTensor(np.array(X_dup)), y_dup
+
+    def train(self, X_train, y_train, n_components=150):
+        # introspect the images arrays to find the shapes (for plotting)
+        X_train = self.rgb2gray(X_train)
+        print("Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0]))
+        t0 = time()
+        pca = PCA(n_components=n_components, svd_solver="randomized", whiten=False).fit(X_train)
+        print("done in %0.3fs" % (time() - t0))
+        self.pca = pca
+        self.eigenfaces = pca.components_.reshape((n_components, self.h, self.w))
+
+        X_train_pca = pca.transform(X_train)
+        # X_test_pca = pca.transform(X_test)
+        # Train a SVM classification model
+
+        print("Fitting the classifier to the training set")
+        t0 = time()
+        param_grid = {
+            "C": loguniform(1e3, 1e4),
+            "gamma": loguniform(1e-4, 1e-2),
+        }
+        clf = RandomizedSearchCV(
+            SVC(kernel="rbf", class_weight="balanced"), param_grid, n_iter=50
+        )
+        X_train_pca_dup, y_train_dup = self.fit4clf_cross_validation(X_train_pca, y_train)
+        self.clf = clf.fit(X_train_pca_dup, y_train_dup)
+        print("done in %0.3fs" % (time() - t0))
+        print("Best estimator found by grid search:")
+        print(self.clf.best_estimator_)
+
+    def predict(self, X_test, y_test):
+        X_test_pca = self.pca.transform(self.rgb2gray(X_test))
+        y_pred = self.clf.predict(X_test_pca)
+        print(y_pred.shape)
+        # print(classification_report(y_test, y_pred))
+        ConfusionMatrixDisplay.from_estimator(
+            self.clf, X_test_pca, y_test, xticks_rotation="vertical"
+        )
+        plt.tight_layout()
+        plt.show()
+        return y_pred
+
+    def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+        """Helper function to plot a gallery of portraits"""
+        plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+        plt.subplots_adjust(bottom=0, left=0.01, right=0.99, top=0.90, hspace=0.35)
+        for i in range(n_row * n_col):
+            plt.subplot(n_row, n_col, i + 1)
+            plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+            plt.title(titles[i], size=12)
+            plt.xticks(())
+            plt.yticks(())
+
+    def show_eigenfaces(self):
+        eigenface_titles = ["eigenface %d" % i for i in range(self.eigenfaces.shape[0])]
+        _, h, w = self.eigenfaces.shape
+        self.plot_gallery(self.eigenfaces, eigenface_titles, h, w)
+        plt.show()
 
 
 class GaussianDistribution:
